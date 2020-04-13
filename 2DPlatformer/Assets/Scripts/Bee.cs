d33d0827ca8712598133fa2fbb8c1d83be3725dd
GabrielHamaoui <62940031+GabrielHamaoui@users.noreply.github.com>
2020-04-13 11:45:19 -0400

    Final enemies prefabs
    last enemies prefab added

---
+++ 2DPlatformer/Assets/Scripts/Bee.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+    GameObject player;
+    Transform target; // player position
+
+    [SerializeField]
+    float agroRange;
+
+    [SerializeField]
+    float moveSpeed;
+
+    int dmgValue = 20;
+
+    Rigidbody2D rb;
+
+    Vector2 movement;
+
+    Animator beeAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = player.transform;
+        beeAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+        direction.Normalize();
+        if (angle < 90 && angle > -90)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        movement = direction;
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        moveEnemy(movement);
+    }
+
+    void moveEnemy(Vector2 direction)
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, target.position);
+        if (distanceToPlayer < agroRange)
+        {
+            rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0);
+        }
+    }
+
+
+    // on collision with player, if not from head, take damage =, i fon head destroy eagle
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // they collide, if player on top, destroy object if not take damage
+            if (target.position.y - transform.position.y > 0)
+            {
+                beeAnim.Play("Enemy_Death");
+                Destroy(this.gameObject, 0.5f);
+                GameStatus.AddScore(25);
+            }
+            else
+            {
+                GameStatus.Damage(dmgValue);
+                // knockback too if possible
+            }
+        }
+    }
+}
---
+++ 2DPlatformer/Assets/Scripts/Slug.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slug : MonoBehaviour
+{
+    GameObject player;
+    Transform target; // player position
+    Animator slugAnim;
+
+    public float speed;
+
+    private bool movingright = true;
+
+    public Transform groudDetection;
+    public Transform wallDetection;
+
+    int dmgValue = 5;
+
+    LayerMask groundWallLayer;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = player.transform;
+        slugAnim = GetComponent<Animator>();
+        groundWallLayer = LayerMask.GetMask("Ground");
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * -speed * Time.deltaTime);
+
+        RaycastHit2D groundCheck = Physics2D.Raycast(groudDetection.position, Vector2.down, 2f);
+
+        if (groundCheck.collider == false)
+        {
+            if (movingright == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingright = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingright = true;
+            }
+        }
+
+        RaycastHit2D wallCheck = Physics2D.Raycast(wallDetection.position, Vector2.down, 0.02f, groundWallLayer);
+
+        if (wallCheck.collider == true)
+        {
+            if (movingright == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingright = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingright = true;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // they collide, if player on top, destroy object if not take damage
+            if (target.position.y - transform.position.y > 0)
+            {
+                slugAnim.Play("Enemy_Death");
+                Destroy(this.gameObject, 0.5f);
+                GameStatus.AddScore(25);
+            }
+            else
+            {
+                GameStatus.Damage(dmgValue);
+                // knockback too if possible
+            }
+        }
+    }
+
+}
