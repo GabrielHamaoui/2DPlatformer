0c1240f7896906e0a38119d5e57bf8a04238184f
jessfatal <jessfatal1@gmail.com>
2020-04-13 10:53:57 -0400

    imported scene

---
+++ 2DPlatformer/Assets/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator myAnimator;
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+    public float jumpHeight = 5f;
+    public bool isGrounded = false;
+
+    private bool facingRight;
+
+    public bool onLadder { get; internal set; }
+
+    void Start()
+    {
+        facingRight = true;
+        myAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Jump();
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontal, 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+        Debug.Log( rb.velocity.y+"");
+
+        Flip(horizontal);
+
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        if (rb.velocity.y == 0)
+        {
+            myAnimator.SetBool("isJumping", false);
+            myAnimator.SetBool("isFalling", false);
+            myAnimator.SetBool("isGrounded", true);
+
+        }
+
+        if (rb.velocity.y > 0)
+        {
+            myAnimator.SetBool("isJumping", true);
+            myAnimator.SetBool("isSkipping", false);
+            myAnimator.SetBool("isGrounded", false);
+        }
+
+        bool isSkip = myAnimator.GetBool("isGrounded");
+
+        if (rb.velocity.y < 0 &&  !isSkip)
+        {
+            myAnimator.SetBool("isJumping", false);
+            myAnimator.SetBool("isFalling", true);
+            myAnimator.SetBool("isGrounded", false);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+        }
+
+
+    }
+
+    void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+    }
+}
---
+++ 2DPlatformer/Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    public float speed;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public float jumpForce;
+
+    public LayerMask whatIsGround; // to detect the type of surface
+
+    private float jumpTimeCounter; // to limit the hold to jump
+    // public float jumpTime; // how long player can jump for
+    private bool isJumping;
+
+    // for ladder
+    public bool onLadder;
+    public float climbSpeed;
+    private float climbVelocity;
+    private float gravityStore;
+
+    // Using blackthornprod HOW TO MAKE ANIMATION TRANSITIONS & MAKING RUN IDLE AND JUMP2D
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        gravityStore = rb.gravityScale;
+    }
+
+    private void FixedUpdate()
+    {
+        // horizontal movement
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        // for animation from skip to no skip
+        if (moveInput == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+
+        // for character reflection (direction change)
+        if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        // for ladder climb
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) // maybe implement trigger
+        {
+
+            if (onLadder)
+            {
+                rb.gravityScale = 0f;
+
+                climbVelocity = climbSpeed * Input.GetAxisRaw("Vertical");
+
+                rb.velocity = new Vector2(rb.velocity.x, climbVelocity);
+
+                anim.SetBool("onLadder", true);
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isClimbing", true);
+            }
+
+        }
+
+        if (!onLadder)
+        {
+            rb.gravityScale = gravityStore;
+
+            anim.SetBool("onLadder", false);
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isClimbing", false);
+        }
+
+
+
+        // jump movement
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("takeOff");
+            isJumping = true;
+            // jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if (isGrounded == true)
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", false);
+        }
+        else if (rb.velocity.y > 0)
+        {
+            anim.SetBool("isJumping", true);
+            anim.SetBool("isFalling", false);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", true);
+        }
+
+
+        /*        // on key hold jump will increase
+                if (Input.GetKeyDown(KeyCode.Space) && isJumping == true)
+                {
+                    if (jumpTimeCounter > 0)
+                    {
+                        rb.velocity = Vector2.up * jumpForce;
+                        jumpTimeCounter -= Time.deltaTime;
+                    }
+                    else { isJumping = false; }
+
+                }
+                // for double jump (?)
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    isJumping = false;
+                }*/
+
+
+
+    }
+}
